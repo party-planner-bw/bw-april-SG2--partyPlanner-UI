59d0ef8c5890b046302152b598ff1ba07f8bc2b8
shanreed <srtech5set@gmail.com>
2019-04-24 09:13:37 -0700

    added some content changed some styling for mobile

--- components/Menu/menu.js
+++ components/Menu/menu.js
@@ -1,28 +1,52 @@
-//Changes the size and color of nav items on hover and 
-const changeNavTextSize = document.querySelectorAll('nav a');
-changeNavTextSize.forEach(a => {
-    a.addEventListener('mouseenter', (ev) => {
-        ev.target.style.fontSize = '2rem';
-        ev.target.style.color = '#E7EBF7';
+// //Changes the size and color of nav items on hover and 
+// const changeNavTextSize = document.querySelectorAll('nav a');
+// changeNavTextSize.forEach(a => {
+//     a.addEventListener('mouseenter', (ev) => {
+//         ev.target.style.fontSize = '1.2rem';
+//         ev.target.style.color1 = '#E7EBF7';
 
-    });
-});
+//     });
+// });
 
-
-changeNavTextSize.forEach(a => {
-    a.addEventListener('touchstart', (ev) => {
-        ev.target.style.fontSize = '1.2rem';
-        ev.target.style.color = '#E7EBF7';
-
-    });
-});
+// changeNavTextSize.forEach(a => {
+//     a.addEventListener('mouseleave', (ev) => {
+//         ev.target.style.fontSize = '1rem';
+//         ev.target.style.color = '#E7EBF7';
+//     });
+// });
 
 
+// changeNavTextSize.forEach(a => {
+//     a.on('touchstart', (ev) => {
+//         ev.target.style.fontSize = '1.2rem';
+//         ev.target.style.color = '#E7EBF7';
 
-//Changes the size and color of navigation items back to original when mouse is moved off
-changeNavTextSize.forEach(a => {
-    a.addEventListener('mouseleave', (ev) => {
-        ev.target.style.fontSize = '1.8rem';
-        ev.target.style.color = '#E7EBF7';
-    });
-});
+//     });
+// });
+
+
+
+// //Changes the size and color of navigation items back to original when mouse is moved off
+// changeNavTextSize.forEach(a => {
+//     a.on('touchend', (ev) => {
+//         ev.target.style.fontSize = '1rem';
+//         ev.target.style.color = '#E7EBF7';
+
+//     });
+// });
+
+// const toggleMenu = () => {
+    
+//     menuRef.classList.toggle('.nav--open');
+//   }
+  
+  
+//   const menuRef = document.querySelector('.nav');
+  
+  
+//   const menuButton = document.querySelector('.menu-button')
+  
+//   menuButton.addEventListener('click', () => { toggleMenu()
+  
+  
+//   })
