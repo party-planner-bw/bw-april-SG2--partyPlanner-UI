43e0049a273d4e8ba145fe95661f229805848bda
shanreed <srtech5set@gmail.com>
2019-04-25 13:08:57 -0700

    added new styles to buttons

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,16 +1,16 @@
-const newButton = document.querySelectorAll('.btn');
-newButton.forEach(btn => {
-btn.addEventListener('mouseenter',(ev) =>{
-    ev.target.style.backgroundColor = '#E7EBF7';
-    ev.target.style.color = '#520D52';
-})
-});
+// const newButton = document.querySelectorAll('.btn');
+// newButton.forEach(btn => {
+// btn.addEventListener('mouseenter',(ev) =>{
+//     ev.target.style.backgroundColor = '#E7EBF7';
+//     ev.target.style.color = '#520D52';
+// })
+// });
 
 
-newButton.forEach(btn => {
-    btn.addEventListener('mouseleave',(ev) =>{
-        ev.target.style.backgroundColor = '#520D52';
-        ev.target.style.TextColor = '#E7EBF7';
-    })
-    });
+// newButton.forEach(btn => {
+//     btn.addEventListener('mouseleave',(ev) =>{
+//         ev.target.style.backgroundColor = '#520D52';
+//         ev.target.style.TextColor = '#E7EBF7';
+//     })
+//     });
     
