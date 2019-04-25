8fee8a776fcfc32131d7f0a0b34a190e8aa9ed56
shanreed <srtech5set@gmail.com>
2019-04-25 07:24:00 -0700

    added some about page styling and content

--- components/Menu/menu.js
+++ components/Menu/menu.js
@@ -16,37 +16,27 @@
 // });
 
 
+// const changeNavTextSize = document.querySelectorAll('nav a');
+// changeNavTextSize.forEach(a => {
+//     a.addEventListener('mouseenter', (ev) => {
+//         ev.target.style.fontSize = '4rem';
+//         ev.target.style.color = '#FFFFFF';
+
+//     });
+// });
+
+// changeNavTextSize.forEach(a => {
+//     a.addEventListener('mouseleave', (ev) => {
+//         ev.target.style.fontSize = '2.5rem';
+//         ev.target.style.color = '#E7EBF7';
+//     });
+// });
+
 // changeNavTextSize.forEach(a => {
 //     a.on('touchstart', (ev) => {
-//         ev.target.style.fontSize = '1.2rem';
+//         ev.target.style.fontSize = '.5rem';
 //         ev.target.style.color = '#E7EBF7';
 
 //     });
 // });
 
-
-
-// //Changes the size and color of navigation items back to original when mouse is moved off
-// changeNavTextSize.forEach(a => {
-//     a.on('touchend', (ev) => {
-//         ev.target.style.fontSize = '1rem';
-//         ev.target.style.color = '#E7EBF7';
-
-//     });
-// });
-
-// const toggleMenu = () => {
-    
-//     menuRef.classList.toggle('.nav--open');
-//   }
-  
-  
-//   const menuRef = document.querySelector('.nav');
-  
-  
-//   const menuButton = document.querySelector('.menu-button')
-  
-//   menuButton.addEventListener('click', () => { toggleMenu()
-  
-  
-//   })
